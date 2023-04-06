82b9accec1f11d82d9319c813fa20fc61b9c4ff2
Reed HIland <whiland@ewu.edu>
2023-04-06 09:00:57 -0700

    Work done to television

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -2,7 +2,7 @@ package cscd212classes.lab2;
 
 import java.util.Objects;
 
-public class Television {
+public class Television implements Comparable<Television>{
     private final boolean fourK;
     private final String make;
     private final String model;
@@ -11,8 +11,8 @@ public class Television {
     private final boolean smart;
 
     public Television(String make, String model, boolean smart, int screenSize, int resolution) {
-       if (make == null || model == null || screenSize < 32 || resolution < 720){
-           //public IllegalArgumentException();
+       if (make == null || make == null || model == null || screenSize < 32 || resolution < 720 ){
+            throw new IllegalArgumentException("Invalid parameters");
        }
 
         this.make = make;
